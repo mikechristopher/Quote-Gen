1b011cf7e017bcf883ae1e9473634839b0fcd317
Mike Nolan <mikechristopher@gmail.com>
2025-09-03 11:31:37 +0100

    fixing quotegen api issues

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//Create EventListener for on page load 
-window.addEventListener('load', getQuote);
+//Create EventListener for on page load
+window.addEventListener("load", getQuote);
 
 //Create EventListener for button
 document.getElementById("spin").addEventListener("click", getQuote);
@@ -9,24 +9,25 @@ const quoteAuthor = document.getElementById("author");
 const quoteText = document.getElementById("quote");
 
 //Endpoint Definition
-const APIURL = "https://dummyjson.com/quotes";
+const APIURL = "https://dummyjson.com/quotes/random";
 
 function getQuote() {
-    fetch(APIURL)
-        .then(response => response.json())
-        .then(function (data) {
-            //Returns message to console that connection to the API is ok, then populates the Author and text from quote
-            console.log('Quote recieved ok from API');
-            quoteAuthor.innerText = data.author;
-            quoteText.innerText = data.quote;
-        })
-        .catch((error) => {
-            //returns error to console with what the error is and then populates with default quote and replaces copyright for a failure notice to the UI.
-            console.error('Error:', error);
-            quoteAuthor.innerText = 'Marcus Brigstocke';
-            quoteText.innerText = "Computer games don't affect kids; I mean if Pac-Man affected us as kids, we'd all be running around in darkened rooms, munching magic pills and listening to repetitive electronic music.";
-            apiFailed.innerText = "Sorry... API Connection Failed - Only have the 1 default quote available for the moment!"
-            copyright.innerText = ""
-        });
+  fetch(APIURL)
+    .then((response) => response.json())
+    .then(function (data) {
+      //Returns message to console that connection to the API is ok, then populates the Author and text from quote
+      console.log("Quote recieved ok from API");
+      quoteAuthor.innerText = data.author;
+      quoteText.innerText = data.quote;
+    })
+    .catch((error) => {
+      //returns error to console with what the error is and then populates with default quote and replaces copyright for a failure notice to the UI.
+      console.error("Error:", error);
+      quoteAuthor.innerText = "Marcus Brigstocke";
+      quoteText.innerText =
+        "Computer games don't affect kids; I mean if Pac-Man affected us as kids, we'd all be running around in darkened rooms, munching magic pills and listening to repetitive electronic music.";
+      apiFailed.innerText =
+        "Sorry... API Connection Failed - Only have the 1 default quote available for the moment!";
+      copyright.innerText = "";
+    });
 }
-
